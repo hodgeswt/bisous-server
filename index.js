a0967db4d8813633d6aeee3bfbd4f9358f81d4a9
Will Hodges <hodgeswt@users.noreply.github.com>
2022-06-02 09:58:25 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.json());
+
+var users = [];
+
+app.post('/register-user', (req, res) => {
+    let username = req.body.user;
+    users.push(username);
+    res.send(`user ${req.body.user} registered`);
+});
+
+app.get('/list-users', (req, res) => {
+    res.send(users);
+});
+
+app.listen(port, () => {
+    console.log(`bisous server listening on port ${port}!`);
+});
